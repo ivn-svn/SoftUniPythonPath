4ecef9933f73b84c7ece3c77d6bf04a6082c8c73
ivn-svn <isvetllin@gmail.com>
2021-12-11 12:57:01 +0200

    11/12/2021 | Daily commit: O&C experimental

---
+++ Programming Fundamentals with Python/2022/experimental/objects_classes_example_00.py
@@ -0,0 +1,6 @@
+class User:
+    pass
+user1 = User()
+user1.first_name = 'User' # If there are more than 1 words within a field name, put them between underscores.
+user1.last_name = 'Userov'
+print(user1.last_name, user1.first_name)
