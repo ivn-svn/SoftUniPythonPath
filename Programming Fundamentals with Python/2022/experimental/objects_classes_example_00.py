a7054ae47f777092d15154f976d361ecdeac4c0c
ivn-svn <isvetllin@gmail.com>
2021-12-12 18:56:30 +0200

    12/12/2021 | Daily commit Experimental

--- Programming Fundamentals with Python/2022/experimental/objects_classes_example_00.py
+++ Programming Fundamentals with Python/2022/experimental/objects_classes_example_00.py
@@ -4,3 +4,10 @@ user1 = User()
 user1.first_name = 'User' # If there are more than 1 words within a field name, put them between underscores.
 user1.last_name = 'Userov'
 print(user1.last_name, user1.first_name)
+
+class SelfUser:
+    def __int__(self, full_name, birthday): # using the constructor class
+        self.name = full_name
+        self.birthday = birthday #yyyymmdd
+
+user = User("Dave Bowman", "19710315")
