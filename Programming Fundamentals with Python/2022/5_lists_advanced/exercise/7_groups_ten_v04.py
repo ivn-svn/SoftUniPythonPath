1998c6a6f9ccfb0d882ea73167e3dee0138e783a
ivn-svn <isvetllin@gmail.com>
2021-12-14 00:24:50 +0200

    14/12/2021 | Daily commit Lists Advanced Exercise

--- Programming Fundamentals with Python/2022/5_lists_advanced/exercise/7_groups_ten_v04.py
+++ Programming Fundamentals with Python/2022/5_lists_advanced/exercise/7_groups_ten_v04.py
@@ -1,5 +1,18 @@
 numlist = input()
 numsplit = numlist.split(', ')
 listofints = [int(x) for x in numsplit]
-groups_of = {f'List of ' + str(((((z - 1) // 10) * 10) + 10)) + f'\'s':z for z in listofints}
-print(groups_of)
+#groups_of = {f'List of ' + str(((((z - 1) // 10) * 10) + 10)) + f'\'s':z for z in listofints}
+groups_of1 = {}
+listofsth = ''
+for z in listofints:
+    zzz = str(((((z - 1) // 10) * 10) + 10))
+    listofsth = f'Group of ' + zzz + f'\'s'
+    if listofsth in groups_of1:
+      # groups_of1[listofsth] += (f', ' + str(z))
+        groups_of1[listofsth].append(z)
+    elif listofsth not in groups_of1:
+     #   groups_of1[listofsth] = str(z)
+        groups_of1[listofsth] = []
+        groups_of1[listofsth].append(z)
+for values in groups_of1:
+    print(f'{values}: {groups_of1[values]}')
