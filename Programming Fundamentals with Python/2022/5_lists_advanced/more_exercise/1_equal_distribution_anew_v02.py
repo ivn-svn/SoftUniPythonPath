d08a1b149e7e676296279cc3b7d9c316f46d2f21
ivn-svn <isvetllin@gmail.com>
2021-12-16 22:26:08 +0200

    16/12/2021 | Daily commit Lists Advanced More Exercise

---
+++ Programming Fundamentals with Python/2022/5_lists_advanced/more_exercise/1_equal_distribution_anew_v02.py
@@ -0,0 +1,11 @@
+wealthlist = str(input())
+min_wealth = int(input())
+ints_wealthlist = [int(z) for z in wealthlist.split(', ')]
+to_distribute = 0
+abundance = 0
+diff = 0
+socialism_built = False
+redis_list = []
+def socialist_revolution():
+    
+while socialism_built == False:
