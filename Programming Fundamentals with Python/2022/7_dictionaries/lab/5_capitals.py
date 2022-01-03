b008054f5d03a7e76cea4e1e71af593c3f1c6128
SVEN <sven@SVENs-MacBook-Air.local>
2022-01-03 20:33:19 +0200

    03/01/2022 | Daily Commit

---
+++ Programming Fundamentals with Python/2022/7_dictionaries/lab/5_capitals.py
@@ -0,0 +1,10 @@
+# 	Capitals
+# Using a dictionary comprehension, write a program that receives country names on the first line, 
+# separated by comma and space ", ", and their corresponding capital cities on the second line 
+# (again separated by comma and space ", "). Print each country with their capital on a separate 
+# line in the following format: "{country} -> {capital}".
+country = ''
+capital = ''
+
+
+print(f"{country} -> {capital}")
