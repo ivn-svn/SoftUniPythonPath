b05d4c34372d0ab6f642fb84bb40f248440af82d
ivn-svn <isvetllin@gmail.com>
2022-01-24 23:39:06 +0200

    24/01/2022 | Daily Commit

---
+++ Programming Fundamentals with Python/5_lists_advanced/exercise/8_decipher_this_v00.py
@@ -0,0 +1,39 @@
+#     • the second and the last letter are switched (e.g., Holle means Hello)
+#     • the first letter is replaced by its character code (e.g., 72 means H)
+# Input:
+# 82yade 115te 103o
+#
+# Output:
+# Ready set go
+user_input = input().split(" ")
+first_letter = ''
+second_letter = ''
+last_letter = ''
+printable_list = []
+ord_char = ''
+final_str = ''
+concat_str = ''
+def str_repl(second_letter, concat_str, last_letter):
+    second_letter = concat_str[1]
+    last_letter = concat_str[len(concat_str) - 1]
+    print(second_letter, last_letter)
+    concat_str = concat_str.replace(last_letter, second_letter)
+    concat_str = concat_str.replace(second_letter, last_letter)
+
+
+for word in user_input:
+    final_str = ''
+    first_letter = ''
+    last_letter = ''
+    ord_char = ''
+    concat_str = ''
+    for idx in range(0, len(word)):
+        if word[idx].isdigit():
+            first_letter = word[idx]
+            ord_char += first_letter
+        else:
+            final_str += word[idx]
+    concat_str = chr(int(ord_char)) + final_str
+    str_repl(second_letter, concat_str, last_letter)
+    printable_list.append(concat_str)
+print(' '.join(printable_list))
