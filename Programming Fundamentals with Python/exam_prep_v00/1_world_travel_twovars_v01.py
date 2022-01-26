9d731a742c79a5c9071b22d829752e44ac21a963
ivn-svn <isvetllin@gmail.com>
2022-01-26 19:53:34 +0200

    26/01/2022 | Daily Commit

--- Programming Fundamentals with Python/exam_prep_v00/1_world_travel_twovars_v01.py
+++ Programming Fundamentals with Python/exam_prep_v00/1_world_travel_twovars_v01.py
@@ -7,42 +7,41 @@
 initial_string = input()
 #
 final_string = initial_string
+
+
 def perform_action(cmd, final_str):
     cmd_split = cmd.split(':')
     if 'add' in cmd_split[0].lower():
         index = int(cmd_split[1])
         stop_name = cmd_split[2]
         if index <= len(final_str):
-            # stp_lst.insert(index, stop_name)
-            # Reworking the above:
-            final_str = final_str[:index] + stop_name + final_str[index:]
+            frontpart = final_str[:index]
+            backpart = final_str[index:]
+            final_str = frontpart + stop_name + backpart
     elif 'remove' in cmd_split[0].lower():
         index = int(cmd_split[1])
-        index1 = int(cmd_split[2])
+        index1 = int(cmd_split[2]) + 1
         if index and index1 <= len(final_str):
-            # pop_items = stp_lst[index:index1]
-            # Reworking the above:
-            final_str = final_str[:index] + final_str[index:]
-            # for item in pop_items:
-            #     stp_lst.pop(item)        
-    elif 'switch' in cmd_split[0].lower(): #TODO: finish the switch func 
+            iten = final_str[index:index1]
+            final_str = final_str.replace(iten, '')
+    elif 'switch' in cmd_split[0].lower():  # TODO: finish the switch func
         old = cmd_split[1]
         new = cmd_split[2]
-        if old in final_str:  
-            # old_overwrite = stp_lst.index(old)
-            # new_overwrite = stp_lst.index(new)
-            final_str.replace(old, new)  
-                
-    return final_str    
+        if old in final_str:
+            final_str = final_str.replace(old, new)
+
+    return final_str
+
+
 command = ''
 commands = []
-stops_list = ''
+
 #
 while command != "Travel":
     command = input()
     commands.append(command)
-    stops_list = perform_action(command, final_string)
-    print(stops_list)
+    final_string = perform_action(command, final_string)
+    print(final_string)
 
 else:
-    print(f"Ready for world tour! Planned stops: {stops_list}")
+    print(f"Ready for world tour! Planned stops: {final_string}")
