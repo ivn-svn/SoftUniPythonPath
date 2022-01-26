8dc0c778467c281d8ee9c02bb1a53798173527e5
ivn-svn <isvetllin@gmail.com>
2022-01-26 10:38:19 +0200

    25/01/2022 | Daily Commit

---
+++ Programming Fundamentals with Python/experimental/forex_experimental.py
@@ -0,0 +1,4 @@
+>>> from forex_python.converter import CurrencyRates
+>>> c = CurrencyRates()
+>>> c.get_rates('USD')   # you can directly call get_rates('USD')
+{u'IDR': 13625.0, u'BGN': 1.7433, u'ILS': 3.8794, u'GBP': 0.68641, u'DKK': 6.6289, u'CAD': 1.3106, u'JPY': 110.36, u'HUF': 282.36, u'RON': 4.0162, u'MYR': 4.081, u'SEK': 8.3419, u'SGD': 1.3815, u'HKD': 7.7673, u'AUD': 1.3833, u'CHF': 0.99144, u'KRW': 1187.3, u'CNY': 6.5475, u'TRY': 2.9839, u'HRK': 6.6731, u'NZD': 1.4777, u'THB': 35.73, u'EUR': 0.89135, u'NOK': 8.3212, u'RUB': 66.774, u'INR': 67.473, u'MXN': 18.41, u'CZK': 24.089, u'BRL': 3.5473, u'PLN': 3.94, u'PHP': 46.775, u'ZAR': 15.747}
