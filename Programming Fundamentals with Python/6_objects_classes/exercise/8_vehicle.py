e1fa98634e8bb7c2c01d025577f5690676323579
Svn Ivn <isvetllin@gmail.com>
2022-03-11 17:47:06 +0200

    11/03/2022 | Daily commit: fixes to 7 & 8 tasks

--- Programming Fundamentals with Python/6_objects_classes/exercise/8_vehicle.py
+++ Programming Fundamentals with Python/6_objects_classes/exercise/8_vehicle.py
@@ -1,30 +1,36 @@
-import types
-
-
 class Vehicle:
-    def __init__(self, type, model, price, owner=None):
+    def __init__(self, type, model, price: int, owner=None):
         self.type = type
         self.model = model
         self.price = price
+        self.owner = owner
+
     def buy(self, money: int, owner: str):
-        if money > self.price and owner == None:
-            change = money - self.price 
-            self.owner = owner
-            return "Successfully bought a {}. Change: {}".format(self.type, self.change)
-        elif money < self.price and owner == None:
+        change = 0
+        if money >= self.price:
+            if self.owner is None:
+                change = money - self.price
+                self.owner = owner
+                pass
+            return "Successfully bought a {}. Change: {:0.2f}".format(self.type, change)
+        elif money < self.price and self.owner is None:
             return "Sorry, not enough money"
-        elif self.owner != None:
+        elif self.owner is not None:
             return "Car already sold"
-    def sell():
-        if owner != None:
-            owner = None
+
+    def sell(self):
+        if self.owner is not None:
+            self.owner = None
         else:
             return "Vehicle has no owner"
-    def __repr__():
-        if owner != None:
-            "Vehicle has no owner {model} {type} is owned by: {owner}"
+
+    def __repr__(self):
+        if self.owner is not None:
+            return "{} {} is owned by: {}".format(self.model, self.type, self.owner)
         else:
-            return "{model} {type} is on sale: {price}"
+            return "{} {} is on sale: {}".format(self.model, self.type, self.price)
+
+
 vehicle_type = "car"
 model = "BMW"
 price = 30000
