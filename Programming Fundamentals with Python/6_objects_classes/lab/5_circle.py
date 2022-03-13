00a2eb041d993dae144a5c7fb1023b792f31df83
Svn Ivn <isvetllin@gmail.com>
2022-03-13 23:25:50 +0200

    13/03/2022 | Daily commit: objects & classes lab, 5_circle.py

--- Programming Fundamentals with Python/6_objects_classes/lab/5_circle.py
+++ Programming Fundamentals with Python/6_objects_classes/lab/5_circle.py
@@ -5,7 +5,7 @@ class Circle:
 		self.diameter = diameter
 		self.radius = diameter / 2
 
-	def calculate_circumstance(self):
+	def calculate_circumference(self):
 		return Circle.__pi * self.diameter
 
 	def calculate_area(self):
@@ -13,3 +13,10 @@ class Circle:
 
 	def calculate_area_of_sector(self, angle):
 		return (angle/360) * Circle.__pi * self.radius * self.radius
+
+circle = Circle(10)
+angle = 5
+
+print(f"{circle.calculate_circumference():.2f}")
+print(f"{circle.calculate_area():.2f}")
+print(f"{circle.calculate_area_of_sector(angle):.2f}")
---
+++ Programming Fundamentals with Python/8_text_processing/exercise/9_rage_quit.py
@@ -0,0 +1,34 @@
+# Now that you have planned out your tour, you are ready to go! Your next task is to mark all the points on the map that
+# you are going to visit.
+# You will be given a string representing some places on the map. You have to filter only the valid ones. A valid
+# location is:
+#     • Surrounded by "=" or "/" on both sides (the first and the last symbols must match)
+#     • After the first "=" or "/" there should be only letters (the first must be upper-case, other letters could be
+# upper or lower-case)
+#     • The letters must be at least 3
+# Example: In the string "=Hawai=/Cyprus/=Invalid/invalid==i5valid=/I5valid/=i=" only the first two locations are valid.
+# After you have matched all the valid locations, you have to calculate travel points. They are calculated by summing
+# the lengths of all the valid destinations that you have found on the map.
+# In the end, on the first line, print: "Destinations: {destinations joined by ', '}".
+# On the second line, print "Travel Points: {travel_points}".
+
+import re
+
+user_input = input()
+
+pattern = r"=([A-Z][a-z]+)=|\/([A-Z][a-z]+)\/"
+locations = []
+travel_points = 0
+
+matches = re.findall(pattern, user_input)
+for match_ in matches:
+    for match in match_:
+        if match != '':
+            travel_points += len(match)
+            locations.append(match)
+            # print(locations)
+
+printable = ', '.join(locations)
+
+print(f"Destinations: {printable}")
+print(f"Travel Points: {travel_points}")
