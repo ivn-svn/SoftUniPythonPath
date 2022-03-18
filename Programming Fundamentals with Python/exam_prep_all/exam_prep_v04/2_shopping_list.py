c3ddd61ca705bf6ffd8b931fa652e4404a80d786
Svn Ivn <isvetllin@gmail.com>
2022-03-18 22:05:06 +0200

    18/03/2022 | Daily commit: exam prep folder python

--- Programming Fundamentals with Python/exam_prep_all/exam_prep_v04/2_shopping_list.py
+++ Programming Fundamentals with Python/exam_prep_all/exam_prep_v04/2_shopping_list.py
@@ -9,17 +9,6 @@
 # Otherwise, skip this command.
 #     • "Rearrange {item}" - if the grocery exists in the list, remove it from its current position and add it at the
 # end of the list. Otherwise, skip this command.
-# # https://judge.softuni.org/Contests/Practice/Index/2031#1
-# Input
-# You will receive an initial list with groceries separated by an exclamation mark "!".
-# After that, you will be receiving 4 types of commands until you receive "Go Shopping!".
-#     • "Urgent {item}" - add the item at the start of the list.  If the item already exists, skip this command.
-#     • "Unnecessary {item}" - remove the item with the given name, only if it exists in the list. Otherwise,
-# skip this command.
-#     • "Correct {oldItem} {newItem}" - if the item with the given old name exists, change its name with the new one.
-# Otherwise, skip this command.
-#     • "Rearrange {item}" - if the grocery exists in the list, remove it from its current position and add it at the
-# end of the list. Otherwise, skip this command.
 initial_list = input().split("!")
 command = ""
 grocery_list = initial_list
