4b0e5b8e92b849c2a726ea749c99873d154812fb
Svn Ivn <isvetllin@gmail.com>
2022-03-19 23:18:17 +0200

    19/03/2022 | Daily commit: exam prep folder python, 3rd

--- Programming Fundamentals with Python/exam_prep_all/exam_prep_v04/3_moving_target.py
+++ Programming Fundamentals with Python/exam_prep_all/exam_prep_v04/3_moving_target.py
@@ -10,12 +10,10 @@ while cmd_ != "End":
         idx = int(cmd[1])
         power = int(cmd[2])
         if idx < len(init_ints_list):
-            # print(init_ints_list)
             if (init_ints_list[idx] - power) <= 0:
                 init_ints_list.pop(idx)
             else:
                 init_ints_list[idx] -= power
-            # print(init_ints_list)
         else:
             pass
 
@@ -23,32 +21,21 @@ while cmd_ != "End":
         idx = int(cmd[1])
         value = int(cmd[2])
         if idx < len(init_ints_list):
-            # print(init_ints_list)
             init_ints_list.insert(idx, value)
-            # print(init_ints_list)
         else:
             print("Invalid placement!")
     elif action == "Strike":
         idx = int(cmd[1])
         radius = int(cmd[2])
-        # if idx < len(init_ints_list):
-        #     # print(init_ints_list)
-        # for i in range(0, radius):
-        positive = init_ints_list.index(int(init_ints_list[idx + radius + 1]))
-        rnegative = radius * -1
-        negative = init_ints_list.index(int(init_ints_list[idx + rnegative]))
-        if 0 <= negative < len(init_ints_list) and 0 <= positive < len(init_ints_list):
-            stuff_to_remove = init_ints_list[negative:positive]
-            # print(stuff_to_remove)
+        positive = idx + radius + 1
+        negative = radius * -1
+        rnegative = idx + negative
+        if rnegative >= 0 and 0 <= rnegative < len(init_ints_list) and 0 <= positive < len(init_ints_list):
+            stuff_to_remove = init_ints_list[rnegative:positive]
             init_ints_list = [ll for ll in init_ints_list if ll not in stuff_to_remove]
-            # print(init_ints_list)
-        # for removable in stuff_to_remove:
-        #     init_ints_list.remove(removable)
-            # print(init_ints_list)
         else:
             print("Strike missed!")
     cmd_ = input()
 init_ints_list = [str(z) for z in init_ints_list]
 printable = "|".join(init_ints_list)
 print(printable)
-
