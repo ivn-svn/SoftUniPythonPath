ab914110587137512f5bc2647ab390a1f35d7642
Svn Ivn <isvetllin@gmail.com>
2022-03-26 22:54:11 +0200

    26/03/2022 | Daily commit: exam prep folder python

--- Programming Fundamentals with Python/exam_prep_v00/2_emoji_detector.py
+++ Programming Fundamentals with Python/exam_prep_v00/2_emoji_detector.py
@@ -1,27 +1,40 @@
 # https://judge.softuni.org/Contests/Practice/Index/2302#1
 import re
 
-
-def ascii_sum():
-    letter_sums = []
-    letter_sum = 0
-    for match in matches:
-        for letter in matches:
-            letter_sum += ord(letter)
-        letter_sums.append(letter_sum)
-    return letter_sums
-
-
 pattern = r"(?<=::)([A-Z][a-z][a-z]+)(?=::)|(?<=\*\*)([A-Z][a-z][a-z]+)(?=\*\*)"
+originals_pattern = r"(\:\:[A-Z][a-z][a-z]+\:\:)|(\*\*[A-Z][a-z][a-z]+\*\*)"
+patternd = r"(\d)"
 output = []
+cool_threshold = []
+originals_list = []
+sum_cool_threshold = 1
 user_input = input()
 
-matches = re.finditer(pattern, user_input)
+matches = re.findall(pattern, user_input)
+num_matches = re.findall(patternd, user_input)
+originals_matches = re.findall(originals_pattern, user_input)
+for m in matches:
+    for mm in m:
+        if mm:
+            # print(mm)
+            output.append(mm)
 
-for match in matches:
-    output.append(matches)
 
+for num in num_matches:
+    for n in num:
+        # if num:
+        # print(n)
+        cool_threshold.append(int(n))
+        sum_cool_threshold *= int(n)
+# sum_cool_threshold = math.prod(cool_threshold)
 
-print(output)
+for original in originals_matches:
+    for theoriginal in original:
+        if theoriginal:
+            # print(theoriginal)
+            originals_list.append(theoriginal)
 
-# regex to use:
+print(f"Cool threshold: {sum_cool_threshold}")
+cool_ones = "\n".join(output)
+printoriginals = "\n".join(originals_list)
+print(f"{len(output)} emojis found in the text. The cool ones are: \n{printoriginals}")
