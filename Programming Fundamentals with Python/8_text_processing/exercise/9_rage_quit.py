c2993a14841956333c18f42cecb212872c899e56
Svn Ivn <isvetllin@gmail.com>
2022-03-28 10:23:05 +0300

    27/03/2022 | Daily commit: exam prep folder python

--- Programming Fundamentals with Python/8_text_processing/exercise/9_rage_quit.py
+++ Programming Fundamentals with Python/8_text_processing/exercise/9_rage_quit.py
@@ -1,15 +1,28 @@
-# Now that you have planned out your tour, you are ready to go! Your next task is to mark all the points on the map that
-# you are going to visit.
-# You will be given a string representing some places on the map. You have to filter only the valid ones. A valid
-# location is:
-#     • Surrounded by "=" or "/" on both sides (the first and the last symbols must match)
-#     • After the first "=" or "/" there should be only letters (the first must be upper-case, other letters could be
-# upper or lower-case)
-#     • The letters must be at least 3
-# Example: In the string "=Hawai=/Cyprus/=Invalid/invalid==i5valid=/I5valid/=i=" only the first two locations are valid.
-# After you have matched all the valid locations, you have to calculate travel points. They are calculated by summing
-# the lengths of all the valid destinations that you have found on the map.
-# In the end, on the first line, print: "Destinations: {destinations joined by ', '}".
-# On the second line, print "Travel Points: {travel_points}".
-
-
+# Every gamer knows what rage-quitting means. It's when you're just not good enough, and you blame everybody else for
+#     losing a game - you press the CAPS LOCK key on the keyboard and flood the chat with gibberish to show your
+#     frustration.
+# Peter is a gamer, a bad one. When he rage-quits, he wants to be the most annoying kid on his team; he wants something
+# truly spectacular. He asks for your help.
+# He'll give you a series of strings (containing only non-numerical characters) followed by non-negative numbers (N),' \
+#   ' e.g., "a3". You need to convert the letters to uppercase for each string and print it repeatedly N times on the ' \
+#   'console.
+#
+#   In the example, you need to write back "AAA".
+# First, on the output, you should print a statistic of the number of unique symbols used (case-insensitive) in the
+# format: "Unique symbols used {0}".
+# Next, print the rage message itself.
+# The strings and numbers will not be separated by anything. The input will always start with a non-numeric symbol,
+# and for each string, there will be a corresponding number. The input will be given on a single line.
+# Input
+#     • The input data should be read from the console.
+#     • It consists of a single line holding a series of string-number sequences.
+#     • The input data will always be valid. There is no need to check it explicitly.
+# Output
+#     • The output should be printed on the console. It should consist of exactly two lines:
+#         ◦ On the first line, print the number of unique symbols used in the message in the format described above.
+#         ◦ On the second line, print the rage message.
+# Constraints
+#     • The count of string-number pairs will be in the range [1 … 20 000].
+#     • Each string will contain any character except digits. The length of each string will be in the range [1 … 20].
+#     • The repeat count for each string will be an integer in the range [0 … 20].
+#     • Allowed working time for your program: 0.3 seconds. Allowed memory: 64 MB.
--- Programming Fundamentals with Python/exam_prep_v00/2_emoji_detector.py
+++ Programming Fundamentals with Python/exam_prep_v00/2_emoji_detector.py
@@ -9,6 +9,23 @@ cool_threshold = []
 originals_list = []
 sum_cool_threshold = 1
 user_input = input()
+emoji_ascii_val_dict = {}
+cool_ones = ""
+
+def ascii_sum(matches):
+    # letter_sums = []
+    for match in matches:
+        letter_sum = 0
+        # print(match)
+
+        for letter in match:
+            if letter:
+                for l in letter:
+                    # print(l)
+                    letter_sum += ord(l)
+                emoji_ascii_val_dict[letter] = letter_sum
+    return emoji_ascii_val_dict
+
 
 matches = re.findall(pattern, user_input)
 num_matches = re.findall(patternd, user_input)
@@ -18,7 +35,6 @@ for m in matches:
         if mm:
             # print(mm)
             output.append(mm)
-
 
 for num in num_matches:
     for n in num:
@@ -34,7 +50,19 @@ for original in originals_matches:
             # print(theoriginal)
             originals_list.append(theoriginal)
 
+emoji_ascii_val_dict = ascii_sum(matches)
+
 print(f"Cool threshold: {sum_cool_threshold}")
 cool_ones = "\n".join(output)
+
+
+for (k, v) in emoji_ascii_val_dict.items():
+    if v < sum_cool_threshold:
+        for o in originals_list:
+            # print(o)
+            # print(k)
+            if k in o:
+                originals_list.remove(o)
 printoriginals = "\n".join(originals_list)
 print(f"{len(output)} emojis found in the text. The cool ones are: \n{printoriginals}")
+# print(f"Dict: {emoji_ascii_val_dict}")
