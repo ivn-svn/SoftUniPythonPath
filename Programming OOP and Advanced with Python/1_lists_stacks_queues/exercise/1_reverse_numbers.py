1ce35b8bd6bd717a6924999da5c1e47a4dfaf163
ivn-svn <isvetllin@gmail.com>
2022-09-04 23:10:33 +0300

    add exercise tasks 1 & 2

---
+++ Programming OOP and Advanced with Python/1_lists_stacks_queues/exercise/1_reverse_numbers.py
@@ -0,0 +1,22 @@
+#
+# Input	Output
+# 1 2 3 4 5	5 4 3 2 1
+#
+# Write a program that reads a string with N integers from the console, 
+# separated by a single space, and reverses them using a stack. Print the reversed 
+# integers on one line, separated by a single space.
+#
+#n = input()
+from collections import deque
+#
+n = '1 2 3 4 5'
+#
+n_splt = deque(n.split(' '))
+new = []
+new_str = ''
+while n_splt:
+    old = n_splt.pop()
+    new.append(old)
+
+new_str = ' '.join(new)
+print(new_str)
