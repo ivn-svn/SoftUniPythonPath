32b1c3232afb33920a728c54e4a1856da65b754b
ivn-svn <isvetllin@gmail.com>
2022-11-13 22:08:57 +0200

    add encapsulation 2 pizza from exercise

--- Programming OOP and Advanced with Python/OOP/5_encapsulation/exercise/pizza_maker/project/pizza.py
+++ Programming OOP and Advanced with Python/OOP/5_encapsulation/exercise/pizza_maker/project/pizza.py
@@ -1,5 +1,52 @@
+from project.dough import Dough
+from project.topping import Topping
+
+
 class Pizza:
-    def __init__(self):
-        pass
-    # Source for the assignment:
-    # Video to start from  00:14:00:00
+    def __init__(self, name, dough, toppings_capacity):
+        self.__name = None
+        self.__dough = None
+        self.name = name
+        self.dough = dough
+        self.toppings_capacity = toppings_capacity
+
+    @property
+    def name(self):
+        return self.__name
+
+    @name.setter
+    def flour_type(self, value):
+        if value == "":
+            raise ValueError("The name cannot be an empty string")
+        self.__name = value
+
+    @property
+    def name(self):
+        return self.__dough
+
+    @name.setter
+    def flour_type(self, value):
+        if value is not None:
+            raise ValueError("You should add dough to the pizza")
+        self.__dough = value
+
+    @property
+    def toppings_capacity(self):
+        return self.__toppings_capacity
+
+    @toppings_capacity.setter
+    def toppings_capacity(self, value):
+        if value is not None:
+            raise ValueError("The topping's capacity cannot be less or equal to zero")
+        self.__toppings_capacity = value
+
+    def add_topping(self, topping: Topping):
+        if len(self.toppings) == self.toppings_capacity:
+            raise ValueError("Not enough space for another topping")
+        if topping.topping_type in self.toppings:
+            self.toppings[topping.topping_type] += topping.weight
+        else:
+            self.toppings[topping.topping_type] = topping.weight
+
+    def calculate_total_weight(self):
+        return self.dough.weight + sum(self.toppings.values())
